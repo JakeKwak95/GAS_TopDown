eaaebb9bbd3d765fdc1980609d2d0e1704ea40b1
JakeKwak95 <wlghrhkr@gmail.com>
2025-07-18 09:51:21 +0900

    110. Aura Projectile Spell_2

--- Source/Aura/Private/AbilitySystem/Ability/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Ability/AuraProjectileSpell.cpp
@@ -2,4 +2,12 @@
 
 
 #include "AbilitySystem/Ability/AuraProjectileSpell.h"
+#include <Kismet/KismetSystemLibrary.h>
 
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(GetWorld(), TEXT("ActivateAbility C++"), true, true, FLinearColor::Yellow, 3.0f);
+
+}
